fd4d4944ec944e35a7f203148915f0b551f95541
Alex Jahl <ajahl.se@gmail.com>
2014-02-17 11:10:46 +0100

    Initial Commit

---
+++ LAttegotchi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LAttegotchi
+//
+//  Created by Alex Jahl on 17.02.14.
+//  Copyright (c) 2014 Alex Jahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LAttegotchiTests/LAttegotchiTests.m
@@ -0,0 +1,34 @@
+//
+//  LAttegotchiTests.m
+//  LAttegotchiTests
+//
+//  Created by Alex Jahl on 17.02.14.
+//  Copyright (c) 2014 Alex Jahl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LAttegotchiTests : XCTestCase
+
+@end
+
+@implementation LAttegotchiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
