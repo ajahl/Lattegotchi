bed134d85f4d3cffc13f18184f0130827f6f28c7
Christoph Eickhoff <christoph.eickhoff@cs.uni-kassel.de>
2014-02-19 10:02:59 +0100

    added notifikations

--- LAttegotchi/AppDelegate.m
+++ LAttegotchi/AppDelegate.m
@@ -190,4 +190,35 @@
     [self startGame];
 }
 
+- (void) createNotifikation{
+    
+    LAttegotchi * latte = [player.lattegotchies objectAtIndex:1];
+    
+    for (Wish * wish in latte.wishes) {
+        
+    
+    UILocalNotification *localNotif = [[UILocalNotification alloc] init];
+    if (localNotif == nil)
+        return;
+    localNotif.fireDate = [NSDate dateWithTimeIntervalSinceNow: 5];
+    localNotif.timeZone = [NSTimeZone defaultTimeZone];
+    
+	// Notification details
+    localNotif.alertBody = @"super text";
+	// Set the action button
+    localNotif.alertAction = @"View";
+    
+    localNotif.soundName = UILocalNotificationDefaultSoundName;
+    localNotif.applicationIconBadgeNumber = 1;
+    
+	// Specify custom data for the notification
+    NSDictionary *infoDict = [NSDictionary dictionaryWithObject:@"someValue" forKey:@"someKey"];
+    localNotif.userInfo = infoDict;
+    
+	// Schedule the notification
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
+        
+    }
+}
+
 @end
